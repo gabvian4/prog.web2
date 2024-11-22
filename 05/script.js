865620a789086ec462446ce9498069991b4a40bc
Gabriel <160731231+gabvian4@users.noreply.github.com>
2024-11-22 10:12:31 -0300

    Add files via upload

---
+++ 05/script.js
@@ -0,0 +1,11 @@
+
+function chamarAlert(){
+    const frase = document.getElementById("popup").value
+    alert(frase)
+}
+
+function somar2Numeros(){
+    const n1 = document.getElementById("n1").value
+    const n2 = document.getElementById("n2").value
+    alert(Number(n1)+Number(n2))
+}
